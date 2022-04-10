ad2eba883b00c8cda302afe29529ef1e8918471f
sertony <serkov_anton@mail.ru>
2022-04-10 16:10:32 +0300

    revert auth

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -4,33 +4,28 @@
  */
 
 var AuthController = {
-  login: function(req, res) {
-    AuthService.authenticate(
-      req,
-      sails.config.auth,
-      function(err, user) {
+  login: function (req, res) {
+    AuthService.authenticate(req, sails.config.auth, function (err, user) {
+      if (err) {
+        return res.send(
+          err.code || 401,
+          err.message || "Incorrect credentials"
+        );
+      }
 
-        if (err) {
-          return res.send(err.code || 401, err.message || 'Incorrect credentials');
-        }
+      if (!user) {
+        // If there is no error passed then we should have a user object
+        return res.serverError("Could not retrieve user");
+      }
 
-        if (!user) {
-          // If there is no error passed then we should have a user object
-          return res.serverError('Could not retrieve user');
-        }
-
-        const token = AuthToken.issueToken({
-          sub: user.username
-        })
-
-        res.cookie("authToken", token, {httpOnly: true, maxAge: 900000, secure: true})
-
-        return res.json({
-          user: user.username,
-          token: token
-        });
+      return res.json({
+        user: user.username,
+        token: AuthToken.issueToken({
+          sub: user.username,
+        }),
       });
-  }
+    });
+  },
 
   // Logout is handled client-side
 };
